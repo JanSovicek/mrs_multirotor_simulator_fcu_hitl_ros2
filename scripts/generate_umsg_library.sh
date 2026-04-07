a65df373c3e246ed2f5352d97f9d1ec6d88566bb
JanSovicek <slavek.sovicek@gmail.com>
2026-04-07 23:41:26 +0200

    Updated CMakeList and added remove command for old umsg definitions

--- scripts/generate_umsg_library.sh
+++ scripts/generate_umsg_library.sh
@@ -1,3 +1,5 @@
-# Generate umsg_lib based on the HILT message definitions
+#Remove old definitions
+rm -r umsg/umsg_lib
 
+# Generate umsg_lib based on the HILT message definitions
 python3 umsg/umsg/umsg_gen/umsg_gen/umsg_gen.py -d umsg/umsg_def/msg_defs_hitl -o umsg/umsg_lib -l
